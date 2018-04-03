7092ad8a0571d9b5003034ec5747172d5275a48c
xbz <xijie205@163.com>
2018-04-03 17:09:23 +0800

    [resolve #5]add test to cron trigger service (#9)

--- src/cron-trigger-service/test/test.js
+++ src/cron-trigger-service/test/test.js
@@ -1,5 +1,32 @@
+const Holder = require('the-holder')
+const loadItems = require('n3h-load-items')
+const path = require('path')
+
 describe('cron-trigger-service', () => {
-  test('todo', () => {
-    // todo add test
+  let holder = null
+
+  beforeEach(() => {
+    holder = new Holder()
   })
+
+  afterEach(async () => {
+    await holder.close()
+  })
+
+  it('should trigger action correctly', (done) => {
+    process.env.TRIGGER_1 = '*/2 * * * * * | ping'
+    const itemDefs = [
+      ...loadItems({dirname: path.resolve(__dirname, '../items')}),
+      {
+        name: 'test',
+        need: ['natsEx', 'cronTrigger'],
+        build: async ({natsEx}) => {
+          natsEx.on('ping', () => {
+            done()
+          })
+        }
+      }
+    ]
+    holder.load(itemDefs)
+  }, 20 * 1000)
 })
