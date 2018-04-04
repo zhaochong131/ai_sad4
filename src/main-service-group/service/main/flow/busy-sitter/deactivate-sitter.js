b1659e2869f94af5f5a3d8d1ec5d60f6ceb5bc89
xbz <xijie205@163.com>
2018-04-04 15:31:36 +0800

    add turn-off-ad step as a combination (#15)
    * add turn-off-ad step as a combination
    
    * rename and refactor deactivate-sitter
    
    * fix bug of deactivate-sitter

---
+++ src/main-service-group/service/main/flow/busy-sitter/deactivate-sitter.js
@@ -0,0 +1,61 @@
+const {ObjectID} = require('mongodb')
+const buildStep = require('n3h-step-builder')
+const Joi = require('joi')
+const buildValidator = require('n3h-joi-validator')
+
+const topics = {
+  queryAdFields: 'action.fb-ad-account.query-ad-fields',
+  deleteCampaign: 'action.fb-ad-account.delete-campaign',
+  updateEffectiveStatus: 'action.ad.update-effective-status'
+}
+
+module.exports = {
+  need: ['natsEx', 'coll/ad', 'coll/father', 'coll/sitter'],
+  build: ({natsEx, 'coll/ad': adColl, 'coll/father': fatherColl, 'coll/sitter': sitterColl}) => buildStep({
+    natsEx,
+    serviceName: 'main',
+    flowName: 'busy-sitter',
+    stepName: 'deactivate-sitter',
+    follow: {
+      step: 'check-ad-spend',
+      case: 'ok.overspend'
+    },
+    validator: buildValidator({
+      sitter: {
+        _id: Joi.string(),
+        adId: Joi.string(),
+        fatherId: Joi.string(),
+        budget: Joi.number()
+      },
+      spend: Joi.number()
+    }),
+    async handler ({sitter, spend}, message) {
+      const {_id, adId, fatherId, budget} = sitter
+
+      // return remaining budget
+      const remainingBudget = budget - spend
+      await fatherColl.updateOne({
+        _id: ObjectID(fatherId)
+      }, {
+        $inc: {budget: remainingBudget}
+      })
+
+      // delete campaign
+      const ad = await adColl.findOne({_id: adId})
+      await message.call(topics.deleteCampaign, {campaignId: ad.campaignId})
+      const fbAd = await message.call(topics.queryAdFields, {adId, fields: ['effective_status']})
+      await message.call(topics.updateEffectiveStatus, {adId, effectiveStatus: fbAd.effective_status})
+
+      // update sitter status to idle
+      await sitterColl.updateOne({_id: ObjectID(_id)}, {
+        $set: {status: 'idle'},
+        $unset: {
+          budget: true,
+          adId: true
+        }
+      })
+
+      this.emit.ok({sitter})
+    }
+  })
+}
