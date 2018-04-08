90c661765f455e89b64c7f4e815433291567fd96
zhaoyao91 <zhaoyao91@163.com>
2018-04-08 15:50:44 +0800

    update nats-server version

--- src/docker/sandbox/scripts/install-nats.sh
+++ src/docker/sandbox/scripts/install-nats.sh
@@ -1,4 +1,4 @@
-NATS_VERSION=1.0.4
+NATS_VERSION=1.1.0
 
 wget https://github.com/nats-io/gnatsd/releases/download/v${NATS_VERSION}/gnatsd-v${NATS_VERSION}-linux-amd64.zip
 unzip -p gnatsd-v${NATS_VERSION}-linux-amd64.zip gnatsd-v${NATS_VERSION}-linux-amd64/gnatsd > gnatsd
